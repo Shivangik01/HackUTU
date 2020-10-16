7fba726fc54c2c5b6c4e373bb96eb5da9f8698e3
Shivangik01 <shivangi.kochrekar@spit.ac.in>
2020-10-16 20:26:15 +0530

    db issues

--- tpo_website/__init__.py
+++ tpo_website/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-#from flask_login import LoginManager
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'ef7f9c363b428a622a04'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
-#db= SQLAlchemy(app)
-#bcrypt = Bcrypt(app)
-#login_manager = LoginManager(app)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+db= SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from tpo_website import routes
--- tpo_website/routes.py
+++ tpo_website/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, Response, redirect, url_for, flash
-from tpo_website import app
+from tpo_website import app,db,bcrypt
 from tpo_website.forms import *
 from tpo_website.models import *
-#from flask_login import login_user, current_user, logout_user, login_required
+from flask_login import login_user, current_user, logout_user, login_required
 from sqlalchemy import or_
 
 
@@ -11,7 +11,36 @@ from sqlalchemy import or_
 def index():
     return render_template('/index.html')
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     return render_template('/dashboard.html')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(studentname=form.studentname.data,
+                    email=form.email.data,
+                    mobile=form.mobile.data,
+                    sem1=form.sem1.data,
+                    sem2=form.sem2.data,
+                    sem3=form.sem3.data,
+                    sem4=form.sem4.data,
+                    sem5=form.sem5.data,
+                    sem6=form.sem6.data,
+                    backlogs=form.backlogs.data,
+                    juniorcollegemarks=form.juniorcollegemarks.data,
+                    secondarymarks=form.secondarymarks.data,
+                    homeaddress=form.homeaddress.data,
+                    branch=form.branch.data,
+                    year=form.year.data,
+                    uid=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created for {form.studentname.data}!',
+              'success')
+        return redirect(url_for('dashboard'))
+    return render_template('/signup.html', title='SignUp Form', form=form)
