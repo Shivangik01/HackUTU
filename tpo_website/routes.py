9cd157331f9bb1c54caf714d192efcd6e9ca8062
Shivangik01 <shivangi.kochrekar@spit.ac.in>
2020-10-17 09:38:38 +0530

    db works

--- tpo_website/routes.py
+++ tpo_website/routes.py
@@ -1,15 +1,29 @@
 from flask import render_template, request, Response, redirect, url_for, flash
-from tpo_website import app,db,bcrypt
+from tpo_website import app, db, bcrypt
 from tpo_website.forms import *
 from tpo_website.models import *
 from flask_login import login_user, current_user, logout_user, login_required
 from sqlalchemy import or_
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/',methods=['GET', 'POST'])
+@app.route('/index',methods=['GET', 'POST'])
 def index():
-    return render_template('/index.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(uid=form.uid.data).first()
+        if user and bcrypt.check_password_hash(user.password,
+                                               form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash(f'You have successfully logged in!',
+              'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check email and password',
+                  'danger')
+    return render_template('/index.html',title='TPO Login Form', form=form)
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
@@ -22,25 +36,26 @@ def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(studentname=form.studentname.data,
-                    email=form.email.data,
-                    mobile=form.mobile.data,
-                    sem1=form.sem1.data,
-                    sem2=form.sem2.data,
-                    sem3=form.sem3.data,
-                    sem4=form.sem4.data,
-                    sem5=form.sem5.data,
-                    sem6=form.sem6.data,
-                    backlogs=form.backlogs.data,
-                    juniorcollegemarks=form.juniorcollegemarks.data,
-                    secondarymarks=form.secondarymarks.data,
-                    homeaddress=form.homeaddress.data,
-                    branch=form.branch.data,
-                    year=form.year.data,
-                    uid=hashed_password)
+        user = User(uid=form.uid.data,
+        studentname=form.studentname.data,
+        email=form.email.data,
+        mobile=form.mobile.data,
+        sem1=form.sem1.data,
+        sem2=form.sem2.data,
+        sem3=form.sem3.data,
+        sem4=form.sem4.data,
+        sem5=form.sem5.data,
+        sem6=form.sem6.data,
+        backlogs=form.backlogs.data,
+        juniorcollegemarks=form.juniorcollegemarks.data,
+        secondarymarks=form.secondarymarks.data,
+        homeaddress=form.homeaddress.data,
+        branch=form.branch.data,
+        year=form.year.data,
+        password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account has been created for {form.studentname.data}!',
               'success')
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('dashboard')) 
     return render_template('/signup.html', title='SignUp Form', form=form)
