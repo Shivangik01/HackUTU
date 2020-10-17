4535409aaf41c50063c470d01a6071f13f545acb
Shivangik01 <shivangi.kochrekar@spit.ac.in>
2020-10-18 00:14:39 +0530

    need to catch errors

--- tpo_website/models.py
+++ tpo_website/models.py
@@ -8,10 +8,10 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     uid=db.Column(db.String(20), unique=True, nullable=False)
-    studentname = db.Column(db.String(20), unique=True, nullable=False)
-    branch=db.Column(db.String(20), nullable=False)
-    year=db.Column(db.Integer, nullable=False)
-    homeaddress=db.Column(db.String(50), nullable=False)
+    studentname = db.Column(db.String(20), nullable=False)
+    branch=db.Column(db.String(20), nullable=True)
+    year=db.Column(db.Integer, nullable=True)
+    homeaddress=db.Column(db.String(50), nullable=True)
     secondaryboard=db.Column(db.String(30), nullable=True)
     secondarymarks=db.Column(db.String(2), nullable=True)
     juniorcollegeboard=db.Column(db.String(30), nullable=True)
@@ -22,13 +22,14 @@ class User(db.Model, UserMixin):
     sem4=db.Column(db.String(2), nullable=True)
     sem5=db.Column(db.String(2), nullable=True)
     sem6=db.Column(db.String(2), nullable=True)
-#    sem7=db.Column(db.String(2), nullable=True)
-#    sem8=db.Column(db.String(2), nullable=True)
+    sem7=db.Column(db.String(2), nullable=True)
+    sem8=db.Column(db.String(2), nullable=True)
     status=db.Column(db.String(1),nullable=False ,default='S')
-    backlogs=db.Column(db.Integer, nullable=False)
+    backlogs=db.Column(db.Integer, nullable=True)
     email = db.Column(db.String(20), unique=True, nullable=False)
     mobile = db.Column(db.Integer, unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
         return f"User('{self.studentname}' , '{self.email}', '{self.mobile}')"
+
